66318c598874e25f758f424f824b9ce9ff662b92
quediablos <msacitbayraktar@gmail.com>
2026-09-09 16:49:13 +0300

    refactored main.go

---
+++ core/application.go
@@ -0,0 +1,19 @@
+package core
+
+type App struct {
+	Locks        map[string]LockInfo      //Key is the resource id.
+	LockRequests map[string][]LockRequest //Key is the resource id.
+	Cycle        int32                    //Counts each operation handled, in some cycles maintenance work is done.
+}
+
+func NewApp() *App {
+	return &App{
+		Locks:        make(map[string]LockInfo),
+		LockRequests: make(map[string][]LockRequest),
+	}
+}
+
+func (app *App) IncrementCycle() int32 {
+	app.Cycle++
+	return app.Cycle
+}
---
+++ network/listener_http.go
@@ -0,0 +1,64 @@
+package network
+
+import (
+	"OnePiece/core"
+	"OnePiece/message"
+	"fmt"
+	"log"
+	"net"
+)
+
+func ListenHttp(app *core.App) {
+
+	listener, err := net.Listen("tcp", "127.0.0.1:3003")
+	if err != nil {
+		log.Fatalf("Failed to bind to port: %v", err)
+	}
+	defer listener.Close()
+	fmt.Println("Single-threaded server running on http://127.0.0.1:3003...")
+
+	for {
+
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept network: %v", err)
+			continue
+		}
+
+		req, err := ReadHTTPRequest(conn)
+		if err != nil {
+			log.Printf("Failed to parse request: %v", err)
+			conn.Close()
+			continue
+		}
+
+		operation, resourceId, err := req.ParseURL()
+		handleOperation(operation, resourceId, conn, app)
+	}
+}
+
+func handleOperation(operation core.Operation, resourceId string, conn net.Conn, app *core.App) {
+	if operation == core.Lock {
+
+		lock := core.CheckForLock(resourceId, app.Locks)
+
+		if lock != nil && !lock.Expired {
+			//Another client already locked the resource, hold this client until the lock is released.
+			core.RequestLock(conn, resourceId, app.LockRequests)
+		} else {
+
+			//No lock is acquired for the resource id yet, acquire the lock, and finish the connectin.
+			core.AcquireLock(resourceId, app.Locks)
+			ReleaseClientHttp(conn, message.GenerateAcquireLockResponse(resourceId))
+		}
+	} else if operation == core.Unlock {
+
+		waitingOne := core.ReleaseLock(resourceId, app.Locks, app.LockRequests)
+
+		if waitingOne != nil {
+			ReleaseClientHttp(waitingOne, message.GenerateAcquireLockResponse(resourceId))
+		}
+		ReleaseClientHttp(conn, message.GenerateReleaseLockResponse(resourceId))
+	}
+
+}
